d7437fd86c6ba8d3ea3671e811118cae5a6b6c1f
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-01 00:03:03 +0530

    [feat] Home route + theme context provider linked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components";
+import { useTheme } from "./context/ThemeContext";
+import { Home } from "./pages";
 
 function App() {
+  const { theme } = useTheme();
   return (
-    <div className="App">
-      Pomodoro
+    <div className={`App ${theme ? "dark-theme" : "light-theme"}`}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
