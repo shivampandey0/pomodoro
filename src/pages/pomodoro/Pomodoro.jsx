37b47009392bf50f68a94c223693bbabc90d9195
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-06 12:52:25 +0530

    [feat] Short and Long break Timers and Controls

--- src/pages/pomodoro/Pomodoro.jsx
+++ src/pages/pomodoro/Pomodoro.jsx
@@ -5,37 +5,34 @@ import { Button, Header } from "../../components";
 import { useData } from "../../context/DataContext";
 import { FaPlay, FaPause, FaRedo } from "react-icons/fa";
 import useTitle from "../../hooks/useTitle";
+import { formatTime } from "../../utils/utils";
 
 export const Pomodoro = () => {
   const { taskID } = useParams();
-  const { taskList } = useData();
+  const {
+    state: { tasks, shortBreak, longBreak },
+  } = useData();
   const [timer, setTimer] = useState(null);
   const [timerKey, setTimerKey] = useState(0);
   const { setTitle } = useTitle("Marvel Pomodoro");
 
-  const { title, description, time } = taskList.find(
-    (item) => item.id === taskID
-  );
+  const {
+    title,
+    description,
+    time: focusTime,
+  } = tasks.find((item) => item.id === taskID);
+
+  const [timerState, setTimerState] = useState({
+    time: focusTime,
+    mode: "Focus",
+  });
 
   const toggleTimer = () => setTimer((prev) => !prev);
 
-  const children = ({ remainingTime }) => {
-    const mins = Math.floor(remainingTime / 60);
-    const secs = remainingTime % 60;
-
-    let _time = "" + mins + "m : " + (secs < 10 ? "0" : "");
-    _time += "" + secs + "s";
-
-    if (timer) {
-      setTitle(`${_time} | ${title}`);
-    }
-
-    return (
-      <div className="txt-center" role="timer" aria-live="assertive">
-        <h1>{_time}</h1>
-        <h3>{`out of ${time} min`} </h3>
-      </div>
-    );
+  const resetTimer = (_time, mode = "Break") => {
+    setTimerState(() => ({ time: _time, mode: mode }));
+    setTimerKey((prev) => prev + 1);
+    setTimer(null);
   };
 
   return (
@@ -47,13 +44,26 @@ export const Pomodoro = () => {
             <CountdownCircleTimer
               key={timerKey}
               isPlaying={timer}
-              duration={time * 60}
+              duration={timerState.time * 60}
               colors={["#950202", "#e8d393"]}
               size={360}
               strokeWidth={24}
+              onComplete={() => resetTimer(shortBreak)}
+              onUpdate={(remainingTime) =>
+                setTitle(`${formatTime(remainingTime)} | ${timerState.mode}`)
+              }
             >
-              {children}
+              {({ remainingTime }) => (
+                <div className="txt-center" role="timer" aria-live="assertive">
+                  <p className="h2 my-2 txt-primary fw-bold">
+                    {timerState.mode}{" "}
+                  </p>
+                  <p className="h1 my-2">{formatTime(remainingTime)}</p>
+                  <p className="h3 my-2">{`out of ${timerState.time} min`}</p>
+                </div>
+              )}
             </CountdownCircleTimer>
+
             {timer === null ? (
               <Button onClick={() => setTimer(true)} type={"primary"}>
                 <FaPlay /> Start
@@ -66,13 +76,17 @@ export const Pomodoro = () => {
             )}
             <Button
               type={"secondary"}
-              onClick={() => {
-                setTimerKey((prev) => prev + 1);
-                setTimer(true);
-              }}
+              onClick={() => resetTimer(focusTime, "Focus")}
             >
               <FaRedo /> Restart
             </Button>
+
+            <div className="flex-row justify-ev gap-1 w-100">
+              <Button type={"primary"} onClick={() => resetTimer(shortBreak)}>
+                Short Break
+              </Button>
+              <Button onClick={() => resetTimer(longBreak)}>Long Break</Button>
+            </div>
           </section>
           <div>
             <h2 className="fw-bold">{title}</h2>
