16d0c34b8c33ad42bc4ab2e97361b3dbbe89f3ac
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-06 12:51:24 +0530

    [refactor] [feat] useReducer in context

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,19 +1,66 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { v4 as uuid } from "uuid";
 
 const DataContext = createContext();
-
 const useData = () => useContext(DataContext);
 
+const initial = {
+  tasks: [],
+  shortBreak: 5,
+  longBreak: 30,
+};
+
+const reducerFunc = (state, { type, payload }) => {
+  switch (type) {
+    case "SHORT_BREAK":
+      return {
+        ...state,
+        shortBreak: Number(payload),
+      };
+    case "LONG_BREAK":
+      return {
+        ...state,
+        longBreak: Number(payload),
+      };
+
+    case "ADD":
+      return {
+        ...state,
+        tasks: [...state.tasks, { id: uuid(), done: false, ...payload }],
+      };
+
+    case "EDIT":
+      return {
+        ...state,
+        tasks: [...state.tasks].map((_task) => {
+          if (_task.id === payload.id) {
+            return { ...payload };
+          }
+          return _task;
+        }),
+      };
+
+    case "DELETE":
+      return {
+        ...state,
+        tasks: [...state.tasks].filter((item) => item.id !== payload),
+      };
+    default:
+      return state;
+  }
+};
+
 const DataProvider = ({ children }) => {
-  const [taskList, setTaskList] = useState(
-    JSON.parse(localStorage.getItem("tasks")) ?? []
+  const [state, dispatch] = useReducer(
+    reducerFunc,
+    JSON.parse(localStorage.getItem("data")) ?? initial
   );
 
   useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(taskList));
-  }, [taskList]);
+    localStorage.setItem("data", JSON.stringify(state));
+  }, [state]);
   return (
-    <DataContext.Provider value={{ taskList, setTaskList }}>
+    <DataContext.Provider value={{ state, dispatch }}>
       {children}
     </DataContext.Provider>
   );
