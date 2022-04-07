4711d7b3355125ac73e02baae49e0d171442572e
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-07 16:10:39 +0530

    [feat] Task done and undone control

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,13 +1,25 @@
 import { FaEdit, FaRegTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-export const Task = ({ task, editHandler, deleteHandler }) => {
+export const Task = ({ task, editHandler, deleteHandler, checkHandler }) => {
   return (
     <div className="task flex-row align-cntr gap-1 justify-sb my-4 mx-2 px-2 py-2">
+      <input
+        className="mx-2"
+        type="checkbox"
+        id={task.id}
+        checked={task.done}
+        onChange={checkHandler}
+      />
       <Link
         to={`/pomodoro/${task.id}`}
-        className="cursor grow-1 txt-md fw-normal"
-      >{`${task.title}`}</Link>
+        className={`cursor grow-1 txt-md fw-normal ${
+          task.done ? "txt-striked" : ""
+        }`}
+      >
+        {task.title}
+      </Link>
+
       <div>
         <FaEdit onClick={editHandler} className="mx-2 txt-md cursor" />
         <FaRegTrashAlt onClick={deleteHandler} className="mx-2 txt-md cursor" />
